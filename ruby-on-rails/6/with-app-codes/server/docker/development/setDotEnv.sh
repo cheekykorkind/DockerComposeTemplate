8a64ddc4696e3290226ffee478b4f0806934e906
cheekykorkind <cheekorkind@gmail.com>
2019-10-16 09:38:52 +0900

    fix setDotenv

--- ruby-on-rails/6/with-app-codes/server/docker/development/setDotEnv.sh
+++ ruby-on-rails/6/with-app-codes/server/docker/development/setDotEnv.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-UID=$(id -u $USER)
-GID=$(id -g $USER)
-UNAME=$(echo "$USER")
-
 touch .env
-echo "UID=${UID}" >> .env
-echo "GID=${GID}" >> .env
-echo "UNAME=${UNAME}" >> .env
+echo "UID=$(id -u $USER)" >> .env
+echo "GID=$(id -g $USER)" >> .env
+echo "UNAME=$USER" >> .env
